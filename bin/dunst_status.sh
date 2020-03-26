ae3511f72dca9c14d8406a2c1a5dba4784bf05cf
Locke Adams <lockeadams@gmail.com>
2020-03-26 19:58:22 -0400

    Updated progresbar + other minor changes.

--- bin/dunst_status.sh
+++ bin/dunst_status.sh
@@ -2,15 +2,11 @@
 # dunst_status.sh - outputs dunst notification with status information
 
 # get date info
-date="$(date '+%A, %B %d - %I:%M %p')"
+date="$(date '+%A, %b %d - %I:%M %p')"
 
 # get battery info
 bat_cap="$(cat /sys/class/power_supply/BAT1/capacity)"
 bat_status="$(cat /sys/class/power_supply/BAT1/status | awk '{print tolower($0)}')"
-
-# get workspace info
-current_ws="$(( $(xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}') + 1))"
-total_ws="$(xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{print $3}')"
 
 # arbitrary (but unique) message ID so it overwrites properly
 msg_id="76494"
@@ -18,5 +14,4 @@ msg_id="76494"
 # output notification
 dunstify -a "dunst_status.sh" -u normal -r "$msg_id" \
 "${date}" \
-"bat: ${bat_cap}%, ${bat_status} \n\
-on workspace ${current_ws} of ${total_ws}"
+"bat: ${bat_cap}%, ${bat_status}"
--- bin/progressbar.sh
+++ bin/progressbar.sh
@@ -2,15 +2,15 @@
 # progressbar.sh - takes a percent (0-100) and prints a progress bar based on it
 
 percent=$1
-solid=$(((percent + 2) / 5))
-hollow=$((20 - solid))
+solid=$(((percent + 2) / 4))
+hollow=$((25 - solid))
 
 for ((i=0; i<$solid; i++)); do
-    echo -n '# '
+    echo -n '▓'
 done
 
 for ((i=0; i<$hollow; i++)); do
-    echo -n '_ '
+    echo -n '░'
 done
 
 echo
